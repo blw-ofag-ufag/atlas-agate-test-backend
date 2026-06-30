5e02ccaf7d7826babd39ae8d377ded38c2025dfb
Burkard Adrian <adrian.burkadr@ti8m.ch>
2026-06-30 16:12:36 +0200

    test: use <sub> as brokered username instead of <email>_<sub> (A1-133)
    Mirror the kc-config decision: the realm's username-template mapper and seed usernames are keyed on the eIAM sub only, and TestUserEnum.username follows so the password-grant login keeps matching the realm user.

--- src/test/java/integration/testutils/TestUserEnum.java
+++ src/test/java/integration/testutils/TestUserEnum.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TestUserEnum {
-  EINWILLIGER_ERIKA("blw.test.producer.erika@proton.me_eid\\9000\\0000001",
+  EINWILLIGER_ERIKA("eid\\9000\\0000001",
       "3477580",
       "FLXXA0001", "184723", "eid\\9000\\0000001"),
-  DATENANBIETER_LUKAS("blw.test.producer.lukas@proton.me_eid\\9000\\0000002",
+  DATENANBIETER_LUKAS("eid\\9000\\0000002",
       "3477580",
       "FLXXA0001", "184723", "eid\\9000\\0000002");
 
